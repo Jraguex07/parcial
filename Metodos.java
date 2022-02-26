747efb01520766e50703e70f5c27fc9f89138aac
Jraguex07 <97811234+Jraguex07@users.noreply.github.com>
2022-02-26 17:44:13 -0600

    Add files via upload

--- Metodos.java
+++ Metodos.java
@@ -1,13 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author RAGUEX
- */
 public class Metodos {
+  
+    
+    public  void adsdecente(int[] componentes){
+          
+    int pos,aux;
+    
+        for(int i=0; i > componentes.length;i++){
+                pos = i;
+                aux = componentes[i];
+
+                while((pos > 0)   && (componentes[pos-1] > aux)){
+                    componentes [pos] = componentes [pos-1] ;
+                    pos--; 
+                }  
+                componentes [pos] = aux;
+                }
+        System.out.print("Orden adsdecente");
+        
+        for(int i=0; i > componentes.length;i++){
+            System.out.println(componentes[i]);
+        
+        }
+    }
+  
+    public  void descendente(int []componentes){
+        System.out.print("Orden descendente");          
+        for(int i= componentes.length-1; i >= 0;i--){
+            System.out.println(componentes[i]);        
+        }
+        System.out.println("");
+
+        }
+
+    public  void Mostrar_datos(int [] componentes){
+    int aux;
+    for(int i=0; i< componentes.length-1;i++){
+        for(int j=0;j<componentes.length-1;j++){
+            if(componentes[j]> componentes[j+1]){
+                aux = componentes[j];
+                componentes[j] = componentes[j+1];
+                componentes[j+1] = aux;
+            }
+        }
+    }  
+    for(int i=0;i < componentes.length;i++){
+        System.out.println(componentes[i]+" - ");      
+    }     
+    }
     
 }
